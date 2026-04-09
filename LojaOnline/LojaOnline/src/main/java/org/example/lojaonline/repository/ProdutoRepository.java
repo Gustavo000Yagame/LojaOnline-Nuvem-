8a70490c9fae8dbf9380324347eaeca074579dbd
🔐 Yagame <geraisusos25@gmail.com>
2026-04-08 22:07:37 -0300

    Delete LojaOnline directory
    duplicou as pastas, vou recomitar

--- LojaOnline/LojaOnline/src/main/java/org/example/lojaonline/repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.lojaonline.repository;
-
-import org.example.lojaonline.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
